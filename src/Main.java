f09f6f0bd6be4b570a9e79d0b5c7e5a83db1f2e9
Mohammed Sameer <85152286+mohammedsameer70@users.noreply.github.com>
2025-04-27 20:42:53 +0530

    Add files via upload
    Added Initial Project file Structure

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import Model.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static Role.ClinicConsole.*;
+import static Role.PatientConsole.*;
+import static Role.PatientConsole.appointmentsList;
+import static Role.PatientConsole.therapy;
+import static Role.PatientUtil.patientList;
+
+class Main {
+
+    public static void main(String[] args) {
+      System.out.println("Hello world")
+
+}
